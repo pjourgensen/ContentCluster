2305f6dabd69f974d52bf86fc820fcb97e211bdc
Peter Jourgensen <48814862+pjourgensen@users.noreply.github.com>
2019-05-02 16:40:50 -0700

    Update config_generator.py

--- config_generator.py
+++ config_generator.py
@@ -72,3 +72,24 @@ if q3 == True:
     
     with open("merge_movie_data_config.json", 'w') as outfile:
         json.dump(o, outfile)
+
+#load_custs.py
+q4 = get_bool("Would you like to generate a config file for load_custs.py (Y/N)? ")
+if q4 == True:
+    csv_path_merged = get_file_path("Please enter the path to the .csv merged data file: ")
+    cust_data1_path = get_file_path("Please enter a path for the .txt data file: ")
+    cust_data2_path = get_file_path("Please enter a path for the .txt data file: ")
+    cust_data3_path = get_file_path("Please enter a path for the .txt data file: ")
+    cust_data4_path = get_file_path("Please enter a path for the .txt data file: ")
+    out_path = get_file_path("Please enter a path for the output .json file: ")
+    
+    o = {}
+    o["mmp"] = csv_path_merged
+    o["cust1"] = cust_data1_path
+    o["cust2"] = cust_data2_path
+    o["cust3"] = cust_data3_path
+    o["cust4"] = cust_data4_path
+    o["output"] = out_path
+    
+    with open("load_custs_config.json", 'w') as outfile:
+        json.dump(o, outfile)
