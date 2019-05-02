8e4e6529a92374ae7ec4c799d580a250c758121e
Josh Kimmel <joshkimmel16@g.ucla.edu>
2019-05-02 15:25:16 -0700

    Fixing minor bugs in kmeans clustering and movie_cluster script. At this point, I was able to get a successful run with k=5.

--- movies/movie_cluster.py
+++ movies/movie_cluster.py
@@ -4,36 +4,36 @@ Results can be fed into some visualization library
 '''
 
 #imports
-import os
+import sys
 import json
 
 #Input functions for clustering algorithms
 
 def direct_compare(datapoint, mean):
     cost = 0
-    for x in range(len(datapoint)):
-        if not datapoint[x] == mean[x]:
+    for x in range(len(datapoint["genres"])):
+        if not datapoint["genres"][x] == mean[x]:
             cost = cost + 1
     return cost
 
 def direct_diff(datapoint, mean):
     cost = 0
-    for x in range(len(datapoint)):
-        cost = cost + (abs(datapoint[x] - mean[x]))
+    for x in range(len(datapoint["genres"])):
+        cost = cost + (abs(datapoint["genres"][x] - mean[x]))
     return cost
 
-def arith_mean(datapoints, default_length=25):
+def arith_mean(datapoints, default_length=20):
     mean = []
     if len(datapoints) == 0:
         for x in range(default_length):
             mean.append(0)
         return mean
     else:
-        length = len(datapoints[0])
+        length = len(datapoints[0]["genres"])
         for x in range(length):
             total = 0
             for datapoint in datapoints:
-                total = total + datapoint[x]
+                total = total + datapoint["genres"][x]
             mean.append(total / len(datapoints))
         return mean
     
@@ -43,15 +43,15 @@ def intra_error_only(intra_error, inter_error):
 def diff_error(intra_error, inter_error):
     return intra_error - inter_error
 
-func_list = {"directCompare": direct_compare, "directDiff": direct_diff, "arithMean": arith_mean, "intraErrorOnly": intra_error_only, "interErrorOnly": inter_error_only}
+func_list = {"directCompare": direct_compare, "directDiff": direct_diff, "arithMean": arith_mean, "intraErrorOnly": intra_error_only, "diffError": diff_error}
 
 #load config
 data_file_path = ""
 algorithm = ""
 alg_options = {}
-with open('../config/movie_category_config.json') as json_file:  
+with open('../config/movie_cluster_config.json') as json_file:  
     data = json.load(json_file)
-    data_file_path = data["data"]
+    data_file_path = data["data_file_path"]
     algorithm = data["algorithm"]
     alg_options = data["alg_options"]
     
@@ -59,15 +59,17 @@ with open('../config/movie_category_config.json') as json_file:
 sys.path.insert(0, r'..\utils')
 from kmeans import KMeans
 
-dataset = json.loads(data_file_path)
+dataset = None
+with open(data_file_path) as json_file:
+    dataset = json.load(json_file)
 
 if algorithm == "kmeans":
     results = {}
-    cost_func = func_list[alg_options["kmeans"]["costFunc"]]
-    mean_func = func_list[alg_options["kmeans"]["meanFunc"]]
-    error_func = func_list[alg_options["kmeans"]["errorFunc"]]
-    for k in xrange(alg_options["kmeans"]["minK"], alg_options["kmeans"]["maxK"]):
-        run = KMeans(dataset, k, cost_func, mean_func, error_func)
-        results[k] = run
+    cost_func = func_list[alg_options["costFunc"]]
+    mean_func = func_list[alg_options["meanFunc"]]
+    error_func = func_list[alg_options["errorFunc"]]
+    for k in range(alg_options["minK"], alg_options["maxK"] + 1):
+        r = KMeans(dataset, k, cost_func, mean_func, error_func)
+        results[k] = r.run()
 else:
     print ("The algorithm specified has not been implemented yet!")
