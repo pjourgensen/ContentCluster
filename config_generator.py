ecbc3bc552c3be93319ad6172aceb6d61d1ec561
Peter Jourgensen <48814862+pjourgensen@users.noreply.github.com>
2019-05-08 18:10:34 -0700

    Update config_generator.py

--- config_generator.py
+++ config_generator.py
@@ -93,3 +93,18 @@ if q4 == True:
     
     with open("load_custs_config.json", 'w') as outfile:
         json.dump(o, outfile)
+
+#load_custs.py
+q5 = get_bool("Would you like to generate a config file for process_cust.py (Y/N)? ")
+if q5 == True:
+    csv_path_merged = get_file_path("Please enter the path to the .csv merged data file: ")
+    cust_data_path = get_file_path("Please enter a path for the .json data file: ")
+    out_path = get_file_path("Please enter a path for the output .csv file: ")
+    
+    o = {}
+    o["mi"] = csv_path_merged
+    o["cd"] = cust_data1_path
+    o["out"] = out_path
+    
+    with open("load_custs_config.json", 'w') as outfile:
+        json.dump(o, outfile)
